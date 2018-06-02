0fa08b1a670c46e8ad20ff4dc4e18985247b410a
Ghost <daniel.sendmemail@gmail.com>
2018-06-02 10:26:39 -0500

    dinamic page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
 import Layout from '../comps/MyLayout'
 import Link from 'next/link'
 
+function getPosts () {
+  return [
+    { 'id': "Hello Next.js", 'title': "Hello Next.js" },
+    { 'id': "learn-nextjs", 'title': "Learn Next.js is awesome" },
+    { 'id': "deploy-nextjs", 'title': "Deploy apps with Zeit" },
+  ]
+}
+
+
+
 const PostLink = (props) => (
   <li>
-    <Link prefetch as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link prefetch as={`/p/${props.post.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.post.title}</a>
     </Link>
+    <style jsx>{`
+      a {
+        font-family: "Arial";
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }  
+    `}</style>
   </li>
 )
 
@@ -13,10 +42,36 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink id="Hello Next.js" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />            
+      {
+        getPosts().map((post) => (
+          <PostLink key={post.id} post={post} />
+        ))
+      }          
     </ul>
+
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }  
+    `}</style>
   </Layout>
 )
 
--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,41 @@
 import Layout from '../comps/MyLayout'
-
-const Content = (props) => (
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-)
-
+import Markdown from 'react-markdown'
 
 export default (props) => (
   <Layout>
-    <Content url={props.url} />
+    <h1>{props.url.query.title}</h1>
+    
+    <div className="markdown">
+      <Markdown source={`
+        This is our blog post.
+        Yes. we can have a.
+        And we can have a title as well.
+
+        ### This is a title
+
+        And here's the content.
+      
+      `}/>
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: red;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 )
